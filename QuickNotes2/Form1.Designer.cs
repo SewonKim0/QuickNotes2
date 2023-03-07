e52866d675c8b699e0afa96e42f27a73c486c722
Sewon Kim <sewonkim0black@gmail.com>
2023-03-07 14:09:41 -0500

    Add project files.

---
+++ QuickNotes2/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace QuickNotes2
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Doc = new System.Windows.Forms.RichTextBox();
+            this.TitleBox = new System.Windows.Forms.TextBox();
+            this.FontSize = new System.Windows.Forms.NumericUpDown();
+            this.LoadBox = new System.Windows.Forms.ComboBox();
+            this.OptionGroup = new System.Windows.Forms.GroupBox();
+            this.DeleteBox = new System.Windows.Forms.ComboBox();
+            this.OptionButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.FontSize)).BeginInit();
+            this.OptionGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Doc
+            // 
+            this.Doc.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Doc.BackColor = System.Drawing.Color.Black;
+            this.Doc.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Doc.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Doc.ForeColor = System.Drawing.Color.White;
+            this.Doc.Location = new System.Drawing.Point(0, 24);
+            this.Doc.Name = "Doc";
+            this.Doc.Size = new System.Drawing.Size(317, 191);
+            this.Doc.TabIndex = 0;
+            this.Doc.TabStop = false;
+            this.Doc.Text = "";
+            this.Doc.TextChanged += new System.EventHandler(this.Doc_TextChanged);
+            // 
+            // TitleBox
+            // 
+            this.TitleBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TitleBox.BackColor = System.Drawing.Color.Black;
+            this.TitleBox.ForeColor = System.Drawing.Color.White;
+            this.TitleBox.Location = new System.Drawing.Point(0, -1);
+            this.TitleBox.Name = "TitleBox";
+            this.TitleBox.Size = new System.Drawing.Size(195, 26);
+            this.TitleBox.TabIndex = 2;
+            this.TitleBox.TabStop = false;
+            // 
+            // FontSize
+            // 
+            this.FontSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.FontSize.BackColor = System.Drawing.Color.Black;
+            this.FontSize.ForeColor = System.Drawing.Color.White;
+            this.FontSize.Location = new System.Drawing.Point(276, -1);
+            this.FontSize.Name = "FontSize";
+            this.FontSize.Size = new System.Drawing.Size(41, 26);
+            this.FontSize.TabIndex = 4;
+            this.FontSize.TabStop = false;
+            this.FontSize.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.FontSize.ValueChanged += new System.EventHandler(this.FontSize_ValueChanged);
+            // 
+            // LoadBox
+            // 
+            this.LoadBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadBox.BackColor = System.Drawing.Color.Black;
+            this.LoadBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoadBox.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadBox.ForeColor = System.Drawing.Color.White;
+            this.LoadBox.FormattingEnabled = true;
+            this.LoadBox.Location = new System.Drawing.Point(18, 21);
+            this.LoadBox.Name = "LoadBox";
+            this.LoadBox.Size = new System.Drawing.Size(166, 27);
+            this.LoadBox.TabIndex = 5;
+            this.LoadBox.TabStop = false;
+            this.LoadBox.Text = "Load";
+            this.LoadBox.SelectedIndexChanged += new System.EventHandler(this.LoadBox_SelectedIndexChanged);
+            // 
+            // OptionGroup
+            // 
+            this.OptionGroup.Controls.Add(this.DeleteBox);
+            this.OptionGroup.Controls.Add(this.LoadBox);
+            this.OptionGroup.Location = new System.Drawing.Point(59, 69);
+            this.OptionGroup.Name = "OptionGroup";
+            this.OptionGroup.Size = new System.Drawing.Size(200, 95);
+            this.OptionGroup.TabIndex = 6;
+            this.OptionGroup.TabStop = false;
+            this.OptionGroup.Text = "OptionGroup";
+            this.OptionGroup.Visible = false;
+            // 
+            // DeleteBox
+            // 
+            this.DeleteBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.DeleteBox.BackColor = System.Drawing.Color.Black;
+            this.DeleteBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DeleteBox.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteBox.ForeColor = System.Drawing.Color.White;
+            this.DeleteBox.FormattingEnabled = true;
+            this.DeleteBox.Location = new System.Drawing.Point(18, 54);
+            this.DeleteBox.Name = "DeleteBox";
+            this.DeleteBox.Size = new System.Drawing.Size(166, 27);
+            this.DeleteBox.TabIndex = 6;
+            this.DeleteBox.TabStop = false;
+            this.DeleteBox.Text = "Delete";
+            this.DeleteBox.SelectedIndexChanged += new System.EventHandler(this.DeleteBox_SelectedIndexChanged);
+            // 
+            // OptionButton
+            // 
+            this.OptionButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.OptionButton.BackColor = System.Drawing.Color.Black;
+            this.OptionButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OptionButton.ForeColor = System.Drawing.Color.White;
+            this.OptionButton.Location = new System.Drawing.Point(194, -1);
+            this.OptionButton.Name = "OptionButton";
+            this.OptionButton.Size = new System.Drawing.Size(82, 26);
+            this.OptionButton.TabIndex = 7;
+            this.OptionButton.TabStop = false;
+            this.OptionButton.Text = "Options";
+            this.OptionButton.UseVisualStyleBackColor = false;
+            this.OptionButton.Click += new System.EventHandler(this.OptionButton_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(317, 214);
+            this.Controls.Add(this.OptionButton);
+            this.Controls.Add(this.OptionGroup);
+            this.Controls.Add(this.FontSize);
+            this.Controls.Add(this.TitleBox);
+            this.Controls.Add(this.Doc);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Main";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.FontSize)).EndInit();
+            this.OptionGroup.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox Doc;
+        private System.Windows.Forms.TextBox TitleBox;
+        private System.Windows.Forms.NumericUpDown FontSize;
+        private System.Windows.Forms.ComboBox LoadBox;
+        private System.Windows.Forms.GroupBox OptionGroup;
+        private System.Windows.Forms.ComboBox DeleteBox;
+        private System.Windows.Forms.Button OptionButton;
+    }
+}
+
---
+++ QuickNotes2/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickNotes2
+{
+    public partial class Main : Form
+    {
+        private const string Saves = "..\\..\\Saves";
+        private System.Threading.Timer timer;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            this.FormClosing += Closing;
+            TitleBox.KeyPress += GoDown;
+            Doc.LinkClicked += ToLink;
+
+            //data(posX posY sizeX sizeY fontSize)
+
+            string[] data = File.ReadAllText("..\\..\\Data.txt").Split(' ');
+
+            if (data.Length == 5)
+            {
+                this.Location = new Point(int.Parse(data[0]), int.Parse(data[1]));
+                this.Size = new Size(int.Parse(data[2]), int.Parse(data[3]));
+
+                FontSize.Value = int.Parse(data[4]);
+                Doc.Font = new Font("Arial", int.Parse(data[4]), FontStyle.Bold);
+            }
+
+            //prepare load and delete options
+
+            string[] files = Directory.GetFiles(Saves);
+            foreach (string file in files)
+            {
+                LoadBox.Items.Add(file.Substring(file.LastIndexOf('\\') + 1));
+                DeleteBox.Items.Add(file.Substring(file.LastIndexOf('\\') + 1));
+            }
+        }
+
+        private void ToLink(object sender, LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText);
+        }
+
+        private void GoDown(object sender, KeyPressEventArgs e) { 
+            if (e.KeyChar == (char) Keys.Enter)
+            {
+                Doc.Focus();
+            }
+        }
+
+        private void Closing(object sender, EventArgs e)
+        {
+            //Save form data
+            //data(posX posY sizeX sizeY fontSize)
+            string formData = "";
+            formData += this.Location.X + " ";
+            formData += this.Location.Y + " ";
+            formData += this.Size.Width + " ";
+            formData += this.Size.Height + " ";
+            formData += FontSize.Value;
+
+            File.WriteAllText("..\\..\\Data.txt", formData);
+
+            //Save
+
+            if (!TitleBox.Text.Equals(""))
+            {
+                //save
+                File.WriteAllText(Saves + "\\" + TitleBox.Text, Doc.Text);
+            }
+            else
+            {
+                if (Doc.Text.Equals(""))
+                {
+                    return;
+                }
+
+                //confirm, save untitled
+
+                DialogResult result = MessageBox.Show("Do you want to save?", "Save", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    int saveNum = 0;
+                    string savePath = Saves + "\\Untitled" + saveNum;
+
+                    while (Directory.GetFiles(Saves).Contains(savePath))
+                    {
+                        saveNum++;
+                        savePath = Saves + "\\Untitled" + saveNum;
+                    }
+
+                    File.WriteAllText(savePath, Doc.Text);
+                }
+            }
+        }
+
+        private void FontSize_ValueChanged(object sender, EventArgs e)
+        {
+            Doc.Font = new Font("Arial", (float)FontSize.Value, FontStyle.Bold);
+        }
+
+        private void Doc_TextChanged(object sender, EventArgs e)
+        {
+            if (timer != null)
+            {
+                timer.Dispose();
+            }      
+            timer = new System.Threading.Timer(new TimerCallback(Reload), null, 3000, 1000);
+        }
+
+        private void Reload(object state)
+        {
+            this.Invoke(new Action(() => { Doc.ReadOnly = true; }));
+
+            int selectionStart = 0;
+            int selectionLength = 0;
+
+            this.Invoke(new Action(() => { selectionStart = Doc.SelectionStart; }));
+            this.Invoke(new Action(() => { selectionLength = Doc.SelectionLength; }));
+
+            string[] lines = null;
+            this.Invoke(new Action(() => { lines = Doc.Text.Split('\n'); }));
+            this.Invoke(new Action(() => { Doc.Text = ""; }));
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+
+                if (line.Length >= 2 && line[0] == '\t' && line[1] == '\t')
+                {
+                    this.Invoke(new Action(() => { Doc.SelectionColor = Color.FromArgb(80, 80, 80); }));                  
+                }
+                else if (line.Length >= 1 && line[0] =='\t')
+                {
+                    this.Invoke(new Action(() => { Doc.SelectionColor = Color.FromArgb(150, 150, 150); }));
+                }
+                else
+                {
+                    this.Invoke(new Action(() => { Doc.SelectionColor = Color.White; }));
+                }
+
+                if (i == lines.Length - 1)
+                {
+                    this.Invoke(new Action(() => { Doc.AppendText(line); }));
+                }
+                else
+                {
+                    this.Invoke(new Action(() => { Doc.AppendText(line + "\n"); }));
+                }      
+            }
+
+            this.Invoke(new Action(() => { Doc.SelectionStart = selectionStart; }));     
+            this.Invoke(new Action(() => { Doc.SelectionLength = selectionLength; }));
+            this.Invoke(new Action(() => { Doc.ScrollToCaret(); }));
+
+            timer.Dispose();
+            this.Invoke(new Action(() => { Doc.ReadOnly = false; }));
+        }
+
+        private void OptionButton_Click(object sender, EventArgs e)
+        {
+            if (OptionGroup.Visible == false)
+            {
+                OptionGroup.Visible = true;
+
+                LoadBox.Text = "Load";
+                DeleteBox.Text = "Delete";
+            }
+            else
+            {
+                OptionGroup.Visible = false;
+            }
+        }
+
+        private void LoadBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //validate input
+            
+            if (!Directory.GetFiles(Saves).Contains(Saves + "\\" + LoadBox.Text))
+            {
+                MessageBox.Show(Directory.GetFiles(Saves)[0]);
+                MessageBox.Show(Saves + "\\" + LoadBox.Text);
+                MessageBox.Show("Error: Invalid file name.");
+                return;
+            }
+
+            //save currDoc
+            
+            if (!TitleBox.Text.Equals(""))
+            {
+                //save
+                File.WriteAllText(Saves + "\\" + TitleBox.Text, Doc.Text);
+
+                //update load/delete options
+
+                if (!LoadBox.Items.Contains(TitleBox.Text))
+                {
+                    LoadBox.Items.Add(TitleBox.Text);
+                    DeleteBox.Items.Add(TitleBox.Text);
+                }
+            }
+            else if (!Doc.Text.Equals(""))
+            {
+                //confirm, save untitled
+
+                DialogResult result = MessageBox.Show("Do you want to save?", "Save", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    int saveNum = 0;
+                    string savePath = Saves + "\\Untitled" + saveNum;
+
+                    while (Directory.GetFiles(Saves).Contains(savePath))
+                    {
+                        saveNum++;
+                        savePath = Saves + "\\Untitled" + saveNum;
+                    }
+
+                    File.WriteAllText(Saves + "\\" + savePath, Doc.Text);
+
+                    //update load/delete options
+
+                    if (!LoadBox.Items.Contains(TitleBox.Text))
+                    {
+                        LoadBox.Items.Add(TitleBox.Text);
+                        DeleteBox.Items.Add(TitleBox.Text);
+                    }
+                }
+            }
+
+            OptionGroup.Visible = false;
+
+            //load into doc
+
+            TitleBox.Text = LoadBox.Text;
+            Doc.Text = File.ReadAllText(Saves + "\\" + LoadBox.Text);
+        }
+
+        private void DeleteBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //confirm
+
+            DialogResult confirm = MessageBox.Show("Do you want to delete (" + DeleteBox.Text + ")?", "Confirm", MessageBoxButtons.YesNo);
+            if (confirm == DialogResult.No)
+            {
+                return;
+            }
+
+            //validate input
+
+            if (!Directory.GetFiles(Saves).Contains(Saves + "\\" + DeleteBox.Text))
+            {
+                MessageBox.Show("Error: Invalid file name.");
+                return;
+            }
+
+            //delete file
+
+            File.Delete(Saves + "\\" + DeleteBox.Text);
+
+            //delete file on doc
+
+            if (TitleBox.Text.Equals(DeleteBox.Text))
+            {
+                TitleBox.Text = "";
+                Doc.Text = "";
+            }
+
+            //delete load/delete entries
+
+            if (LoadBox.Items.Contains(DeleteBox.Text))
+            {
+                LoadBox.Items.Remove(DeleteBox.Text);
+                DeleteBox.Items.Remove(DeleteBox.Text);         
+            }
+        }
+    }
+}
